faf40d647c27ff542977da5365e3b494b65301a4
Erik Hope <erik.hope@chime.com>
2022-11-30 09:34:45 -0800

    delete day1 files

--- src/lib/gen_files.ts
+++ src/lib/gen_files.ts
@@ -1,9 +1,12 @@
+import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
 import * as stream from 'stream';
 import axios from 'axios';
 import {promisify} from 'util';
+
+dotenv.config();
 
 const genFiles = async () => {
   const args = process.argv.slice(2);
@@ -24,7 +27,7 @@ const genFiles = async () => {
   const done = promisify(stream.finished);
 
   const downloadFile = async () => {
-    const resp = await axios({ method: "get", url: inputUrl,responseType: "stream", headers: { Cookie: "session=53616c7465645f5f69c805edae57a9903e03c43e389f01adb918aeb1523072367a4bacfe5eba9dc122baa385a66a427d5d5291b5ecf698636d46e0116c756a25" }});
+    const resp = await axios({ method: "get", url: inputUrl,responseType: "stream", headers: { Cookie: `session=${process.env.AOC_SESSION}` }});
     const stream = fs.createWriteStream(dest);
     resp.data.pipe(stream);
     return done(stream);
