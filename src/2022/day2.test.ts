82abb480fa6c328e11785c08901f3fd72840ca8b
Erik Hope <erik.hope@chime.com>
2022-12-01 21:17:53 -0800

    2022 day 2

---
+++ src/2022/day2.test.ts
@@ -0,0 +1,15 @@
+import {part1, part2} from './day2';
+
+describe("day 2", () => {
+  describe("part1", () => {
+    it("matches the example", () => {
+      expect(part1(true)).toEqual(15);
+    });
+  });
+
+  describe("part2", () => {
+    it("matches the example", () => {
+      expect(part2(true)).toEqual(12);
+    });
+  });
+});
---
+++ src/2022/day2.ts
@@ -0,0 +1,91 @@
+import {loadPuzzleInput} from '../lib/load_file'
+
+/*
+  A: rock
+  B: paper
+  C: scissors
+
+  X: rock
+  Y: paper
+  Z: scissors
+*/
+
+const ties = {
+  'A': 'X',
+  'B': 'Y',
+  'C': 'Z',
+}
+
+const wins = {
+  'A': 'Z',
+  'B': 'X',
+  'C': 'Y',
+}
+
+const loses = {
+  'A': 'Y',
+  'B': 'Z',
+  'C': 'X',
+}
+
+const scores = {
+  'X': 1,
+  'Y': 2,
+  'Z': 3,
+}
+
+function checkVictory(hand1: string, hand2: string) {
+  if (ties[hand1] == hand2) {
+    return 0;
+  } else if (wins[hand1] == hand2) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
+export function part1(example=false) {
+  const inp = loadPuzzleInput("2", example, "2022");
+
+  let total = 0;
+  inp.forEach(line => {
+    const moves = line.split(' ');
+    const result = checkVictory(moves[0], moves[1]);
+    if (result == 1) {
+      total += scores[moves[1]] + 6;
+    } else if (result == 0) {
+      total += scores[moves[1]] + 3;
+    } else {
+      total += scores[moves[1]];
+    }
+  });
+
+  return total;
+}
+
+export function part2(example=false) {
+  const inp = loadPuzzleInput("2", example, "2022");
+
+  let total = 0;
+
+  inp.forEach(line => {
+    const moves = line.split(' ');
+
+    let move = "";
+    if (moves[1] === 'X') {
+      // lose
+      move = wins[moves[0]];
+    } else if (moves[1] === 'Y') {
+      // draw
+      move = ties[moves[0]];
+      total += 3;
+    } else {
+      // win
+      move = loses[moves[0]];
+      total += 6;
+    }
+    total += scores[move];
+  });
+
+  return total;
+}
